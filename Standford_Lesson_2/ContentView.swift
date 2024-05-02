6bfc60f2cb368f7fe6ebf5328a88466a026bed6b
Ravshanbek Tursunbaev <rtursunbaev9822@gmail.com>
2024-05-02 13:57:14 +0500

    Initial Commit

---
+++ Standford_Lesson_2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Standford_Lesson_2
+//
+//  Created by Ravshan Winter on 02/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Standford_Lesson_2/Standford_Lesson_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Standford_Lesson_2App.swift
+//  Standford_Lesson_2
+//
+//  Created by Ravshan Winter on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Standford_Lesson_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
